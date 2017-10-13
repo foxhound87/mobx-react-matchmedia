3a6f3aba89455562e81ce8474c6a69f12fcb1d41
Claudio Savino <claudio.savino@me.com>
2017-10-13 17:17:00 +0200

    feat: support for React 16

--- tests/test.jsx
+++ tests/test.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { expect } from 'chai';
+import { JSDOM } from 'jsdom';
 import { shallow } from 'enzyme';
 import { isObservable } from 'mobx';
-
 import { MatchMediaProvider } from '../src/index';
 import breakpoints from './data/breakpoints';
+import './config';
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = jsdom.window;
+
 
 describe('<MatchMediaProvider />', () => {
   // test
